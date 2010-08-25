e88f46156922ec21f2d258cc16737828559bedb6
Christian Holl <cyborgx1@gmail.com>
2010-08-25 01:07:00 +0000

    ooops

--- QFlukePlotter/QFlukePlotter.cpp
+++ QFlukePlotter/QFlukePlotter.cpp
@@ -1,10 +1,29 @@
 #include "QFlukePlotter.hpp"
 
+
+
+//
+//QFlukePlotCanvas::QFlukePlotCanvas(QwtPlot *plot)
+//  :QwtPlotCanvas(*plot->canvas())
+//{
+//
+//}
+//
+//QFlukePlotCanvas::~QFlukePlotCanvas()
+//{
+//
+//}
+
+
 QFlukePlotter::QFlukePlotter(QWidget *parent)
     : QwtPlot(parent)
 {
 	this->setAxisTitle(2,"Time [s]");
+        connect(this->canvas(),SIGNAL(customContextMenuRequested(QPoint)),this,SLOT(OnCanvasClick()));
+
 }
+
+
 
 
 QFlukePlotter::~QFlukePlotter()
@@ -12,9 +31,10 @@ QFlukePlotter::~QFlukePlotter()
 
 }
 
-
-
-
+void QFlukePlotter::OnCanvasClick()
+{
+	std::cout<<"testtt"<<std::endl;
+}
 
 
 void QFlukePlotter::clear()
--- QFlukePlotter/QFlukePlotter.hpp
+++ QFlukePlotter/QFlukePlotter.hpp
@@ -3,36 +3,68 @@
 
 #include <QtGui/QWidget>
  #include <QWheelEvent>
-#include <qwt-qt4/qwt_plot.h>
 
+#include <qwt-qt4/qwt_plot.h>
+#include <qwt-qt4/qwt_scale_div.h>
+#include <qwt-qt4/qwt_plot_canvas.h>
 #include <iostream> //TODO Remove
 
 #include "Fluke189.hpp"
+
+
+
+//class QFlukePlotCanvas : public QwtPlotCanvas
+//{
+//	Q_OBJECT
+//
+//public:
+//	QFlukePlotCanvas(QwtPlot *plot);
+//    ~QFlukePlotCanvas();
+//
+//    void wheelEvent(QWheelEvent * e)
+//    {
+//    	std::cout<<" X:"<<e->x();
+//    	std::cout<<" Y:"<<e->y();
+//    	std::cout<<" Orient: "<<e->orientation();
+//    	std::cout<<" Delta: "<<e->delta();
+//    	std::cout<<" Dir: ";
+//    	if(e->orientation()==1)//MouseWheel HorizontalDirection
+//    	{
+//    		std::cout<<"h";
+//    	}
+//    	if(e->orientation()==2)//MouseWheel VerticalDirection
+//    	{
+//    		std::cout<<"v";
+//    	}
+//    	std::cout<<std::endl;
+//    }
+//};
+
+
+
 
 class QFlukePlotter : public QwtPlot
 {
     Q_OBJECT
 
 public:
+
     QFlukePlotter(QWidget *parent = 0);
     ~QFlukePlotter();
+
 
 private:
 
     void mouseDoubleClickEvent(QMouseEvent * e)
     {
     	std::cout<<"test"<<std::endl;
+
     }
     void mouseMoveEvent(QMouseEvent * e)
     {
     	std::cout<<"move move"<<std::endl;
     }
-    void wheelEvent(QWheelEvent * e)
-    {
 
-    	std::cout<<e->delta()<<std::endl;
-    	//std::cout<<"down"<<std::endl;
-    }
 
 
 public slots:
@@ -40,6 +72,10 @@ public slots:
 	 * Clears all plot-data
 	 */
 	void clear();
+
+
+
+	void OnCanvasClick();
 
 };
 
